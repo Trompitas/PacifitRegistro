bb1e3c32cbad4ba463c24f8d232db2ad33b0d64a
Trompitas <90296185+Trompitas@users.noreply.github.com>
2022-06-03 21:53:24 -0500

    Add files via upload

---
+++ src/models/usuarioModels.js
@@ -0,0 +1,23 @@
+const {Schema, model} = require("mongoose");
+
+const usuarioSchema = Schema({
+  nombreUsuario: {
+    type: String,
+    required: true,
+  },
+  nombreCompleto:{
+    type: String,
+    required: true,
+  },
+  contrasena: {
+    type: String,
+    required: true,
+  },
+},
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+module.exports = model('Usuario', usuarioSchema);
